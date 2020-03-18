9334ef7e2f18ddc2f811d311a80bb7993b64c9cf
ShawnTseng <shawntseng40@gmail.com>
2020-03-18 10:58:09 +0800

    review variables

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-document.getElementById("title").textContent = "Hi, here is Shawn.";
+let welcome = "Hello World!!!",
+  price = 49.99,
+  discounted = false;
+
+document.getElementById("title").textContent = welcome;
+document.getElementById("price").textContent = price;
+document.getElementById("discount").textContent = discounted;
