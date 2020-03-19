ead1ac17af8d13b4f22561f87378d916dfa497fd
ShawnTseng <shawntseng40@gmail.com>
2020-03-19 13:19:33 +0800

    typeof

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ let welcome = "Hello World!!!",
 document.getElementById("title").textContent = welcome;
 document.getElementById("price").textContent = price;
 document.getElementById("discount").textContent = discounted;
+
+var result = typeof price;
+// result = typeof welcome;
+
+document.getElementById("result").textContent = result;
