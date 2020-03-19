79dfbdab8ffaf374cbf975ef73cd24a625505e21
ShawnTseng <shawntseng40@gmail.com>
2020-03-19 14:14:27 +0800

    null&undefined

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 let count = parseFloat("32");
-
 document.getElementById("result").textContent = count;
 document.getElementById("result-type").textContent = typeof count;
+
+let undefinedValue = undefined;
+document.getElementById("undefined").textContent = undefinedValue;
+document.getElementById("undefined-type").textContent = typeof undefinedValue;
+
+let nullValue = null;
+document.getElementById("null").textContent = nullValue;
+document.getElementById("null-type").textContent = typeof nullValue;
 
 console.log(this);
