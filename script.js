0773842ccf18e0e6f79db70425928ec57484daf7
ShawnTseng <shawntseng40@gmail.com>
2020-03-31 10:56:48 +0800

    falsy

--- script.js
+++ script.js
@@ -17,4 +17,22 @@ let person = {
 document.getElementById("person").textContent = person;
 document.getElementById("person-type").textContent = typeof person;
 
+document.getElementById("boolean").textContent = "boolean";
+document.getElementById("boolean-falsy").textContent = false;
+
+document.getElementById("number").textContent = "number";
+document.getElementById("number-falsy").textContent = !!0;
+
+document.getElementById("string").textContent = "string";
+document.getElementById("string-falsy").textContent = !!"";
+
+document.getElementById("null").textContent = "null";
+document.getElementById("null-falsy").textContent = !!null;
+
+document.getElementById("undefined").textContent = "undefined";
+document.getElementById("undefined-falsy").textContent = !!undefined;
+
+document.getElementById("NaN").textContent = "NaN";
+document.getElementById("NaN-falsy").textContent = !!NaN;
+
 console.log(this);
